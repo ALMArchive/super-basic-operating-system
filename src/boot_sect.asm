215c1e92f195ed174cdad8adc51d20ba40d0c590
ALMaclaine <almaclaine@gmail.com>
2019-12-20 23:47:30 -0800

    update boot_sect to segmentation

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -3,25 +3,26 @@
   ;
   mov ah, 0x0e
 
-  mov bp, 0x8000
-  mov sp, bp
-
-  push 'A'
-  push 'B'
-  push 'C'
-
-  pop bx
-  mov al, bl
+  mov al, [the_secret]
   int 0x10
 
-  pop bx
-  mov al, bl
+  mov bx, 0x7c0
+  mov ds, bx
+  mov al, [the_secret]
   int 0x10
 
-  mov al, [0x7ffe]
+  mov al, [es:the_secret]
+  int 0x10
+
+  mov bx, 0x7c0
+  mov es, bx
+  mov al, [es: the_secret]
   int 0x10
 
   jmp $
+
+the_secret:
+  db "X"
 
   times 510-($-$$) db 0
 

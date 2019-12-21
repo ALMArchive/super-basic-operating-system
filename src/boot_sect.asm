b82891ab03e5b55b12ecdacaa27977b530f7f3ea
ALMaclaine <almaclaine@gmail.com>
2019-12-20 22:47:22 -0800

    revert to basic loop

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -1,20 +1,10 @@
   ;
-  ; A simple boot sector that prints a message to the screen using a BIOS routine.
+  ; A simple boot sector program that loops forever.
   ;
-  mov ah, 0x0e
-  mov al, 'H'
-  int 0x10
-  mov al, 'e'
-  int 0x10
-  mov al, 'l'
-  int 0x10
-  mov al, 'l'
-  int 0x10
-  mov al, 'o'
-  int 0x10
 
-  jmp $
+loop:
+  jmp loop
 
-  times 510-($-$$) db 0
+times 510-($-$$) db 0
 
-  dw 0xaa55
+dw 0xaa55

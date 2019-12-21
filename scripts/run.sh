b04a622931a0ffa14aeda74a6b2a98b8958165fe
ALMaclaine <almaclaine@gmail.com>
2019-12-20 19:32:21 -0800

    add run script

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ ! `command -v qemu-system-x86_64` ]
+then
+  printf "Install qemu\n"
+  exit 1
+fi
+
+if [ ! -f dist/boot_sect.bin ]
+then
+  scripts/compile.sh
+fi
+
+qemu-system-x86_64 dist
